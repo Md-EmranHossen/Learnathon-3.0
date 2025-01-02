cfb4a9367634b14db39c2f98cba06467efd15461
Md. Emran Hossen <emranhossen3075@gmail.com>
2025-01-02 17:59:58 +0600

    Type Conversion and Data Types

---
+++ 01. Fundamentals/TypeConversion/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace TypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 1;
+            int num = b;
+
+            int x = 256;
+            //byte by = x;
+            //byte by = (byte) x;
+            byte by = (byte)x;
+
+            Console.WriteLine(by);
+            Console.WriteLine(num);
+
+
+            var number = "12335";
+            int i = Convert.ToInt32(number);
+            int j = int.Parse(number);
+            Console.WriteLine(j);
+
+
+
+            try
+            {
+                var ID = "21225103064";
+                byte Bt = Convert.ToByte(ID);
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Out of Range");
+            }
+
+
+
+            try
+            {
+                var str = true;
+                bool Status = Convert.ToBoolean(str);
+                Console.WriteLine(Status);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Can not Convert");
+            }
+
+
+        }
+
+    }
+}
---
+++ 01. Fundamentals/VariableAndConstant/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace VariableAndConstant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var length = 4.5;
+            byte Number = 3;
+            int cnt = 10;
+            float price = 34.5f;
+            char ch = 'E';
+            string name = "Emran";
+            const float PI = 3.1416f;
+            Console.WriteLine(name);
+            Console.WriteLine(ch);
+            Console.WriteLine(cnt);
+            Console.WriteLine(price);
+            Console.WriteLine(Number);
+            Console.WriteLine("{0},{1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0},{1}", int.MinValue, int.MaxValue);
+        }
+    }
+}
