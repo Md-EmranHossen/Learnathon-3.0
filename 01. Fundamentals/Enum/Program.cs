63c14458aa69e496fa6d6ce1da69ec6a5a7a7eb6
Md. Emran Hossen <emranhossen3075@gmail.com>
2025-01-03 02:47:54 +0600

    Array,string and enum

---
+++ 01. Fundamentals/Enum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+namespace Enum
+{
+    public enum ShippingMethood
+    {
+        RegularMail = 1,
+        RgirsterdMail = 2,
+        Expose = 3
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var methood = ShippingMethood.Expose;
+
+            Console.WriteLine((int)methood);
+
+            var method1 = 3;
+
+            Console.WriteLine((ShippingMethood)method1);
+
+            Console.WriteLine(methood.ToString());
+
+            var methoodName = "Expose";
+
+            var shippingMethod = (ShippingMethood)Enum.Parse(typeof(ShippingMethood), methodName);
+
+        }
+    }
+}
