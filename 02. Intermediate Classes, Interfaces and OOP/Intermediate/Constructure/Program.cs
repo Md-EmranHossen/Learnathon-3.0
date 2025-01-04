c41855233085bccc9bcdfc5767d5e0a512b8dbe6
Md. Emran Hossen <emranhossen3075@gmail.com>
2025-01-04 22:00:41 +0600

    Classes, Constructors,Object Initializers, Methods,Access Modifires etc

---
+++ 02. Intermediate Classes, Interfaces and OOP/Intermediate/Constructure/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Constructure
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "Emran";
+
+            var order = new Order();
+            customer.Orders.Add(order);
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+        }
+    }
+}
