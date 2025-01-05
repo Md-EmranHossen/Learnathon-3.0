c0c67a80de6617be5c62652562f12aabc19e4c2c
Md. Emran Hossen <emranhossen3075@gmail.com>
2025-01-05 23:57:58 +0600

    add

---
+++ 02. Intermediate Classes, Interfaces and OOP/Intermediate/ConstructorInheritance/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace ConstructorInheritance
+{
+    public class Car : Vehicle
+    {
+        
+        public Car(string registrationNumber)
+            : base(registrationNumber) 
+        {
+            Console.WriteLine("Car is begin initilized. {0}" + registrationNumber);
+        }
+    }
+}
---
+++ 02. Intermediate Classes, Interfaces and OOP/Intermediate/UpCastingAndDownCasting/Text.cs
@@ -0,0 +1,8 @@
+﻿namespace UpCastingAndDownCasting
+{
+    public class Text: Shape
+    {
+        public int FintSize { get; set; }
+        public string FontName { get; set; }
+    }
+}
