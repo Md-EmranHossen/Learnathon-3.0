f34bf15ac97c889f2641455748937c185c8aeed1
Md. Emran Hossen <emranhossen3075@gmail.com>
2025-01-06 17:31:34 +0600

    Generics and Delegates

---
+++ 03. C# Advanced Topic/Advance/Delegates/PhotoProcessor.cs
@@ -0,0 +1,15 @@
+﻿namespace Delegates
+{
+    public class PhotoProcessor
+    {
+       // public delegate void PhotoFilterHandler(Photo photo);
+        public void Process(string path,Action<Photo> filterHandler)
+        {
+            //System.Action<>
+           // System.Func<>
+            var photo = Photo.Load(path);
+            filterHandler(photo);
+            photo.Save();
+        }
+    }
+}
---
+++ 03. C# Advanced Topic/Advance/Generics/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Generics
+{
+    public class GenericList<T>
+    {
+        public void Add(T value)
+        {
+
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+
+    public class GenericDictionary<TKey, Tvalue>
+    {
+        public void Add(TKey key, Tvalue value)
+        {
+
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var numbers = new GenericList<int>();
+            numbers.Add(1);
+
+            var books = new GenericList<Book>();
+            books.Add(new Book());
+
+            var dictionary = new GenericDictionary<string, Book>();
+            dictionary.Add("1234", new Book());*/
+
+            var number = new Nullable<int>(5);
+            Console.WriteLine("Has Value ?" + number.HasValue);
+            Console.WriteLine("value is "  + number.GetValueOrDefault());
+          
+            
+        }
+    }
+}
