357d3a634261125a3a7c3bc7d5ffa24a0383a976
Md. Emran Hossen <emranhossen3075@gmail.com>
2025-01-06 19:14:56 +0600

    LamdaExpression

---
+++ 03. C# Advanced Topic/Advance/LamdaExpression/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace LamdaExpression
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // args => expression
+            // number => number * number
+            //() => ....
+            // x => ....
+            // (x,y,z) => ....
+
+
+            /* Func<int, int> square = number => number * number;
+             Console.WriteLine(square(5));*/
+
+
+            /*
+            const int factor = 5;
+            Func<int,int> multipler = n => n * factor;
+            var result = multipler(10);
+            Console.WriteLine(result);
+            */
+
+
+
+            var books = new BookRepositories().GetBooks();
+            var cheapBooks =  books.FindAll(book => book.Price < 10);
+
+
+            foreach (var book in books) { 
+                Console.WriteLine(book.Title);
+            }
+        }
+    }
+}
