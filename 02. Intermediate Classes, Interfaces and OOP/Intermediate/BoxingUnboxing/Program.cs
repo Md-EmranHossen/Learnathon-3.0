108b2c75caf3b3928f623ed9a2a8464817a3ec7e
Md. Emran Hossen <emranhossen3075@gmail.com>
2025-01-06 06:43:39 +0600

    Add polymorphisom

---
+++ 02. Intermediate Classes, Interfaces and OOP/Intermediate/BoxingUnboxing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace BoxingUnboxing
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           var list = new ArrayList();
+            list.Add(1);
+            list.Add("Emran");
+            list.Add(4.5f);
+            list.Add(DateTime.Now);
+            var number = (int)list[1];
+
+            var anotherList = new List<int>();
+            var names = new List<string>();
+            names.Add("Emran");
+        }
+    }
+}
