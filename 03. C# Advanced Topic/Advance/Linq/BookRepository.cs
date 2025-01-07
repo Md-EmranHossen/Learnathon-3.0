fa59152c9a393e94b97828da040754c8b4e3d5ad
Md. Emran Hossen <emranhossen3075@gmail.com>
2025-01-07 16:53:56 +0600

    NullableTypes

--- 03. C# Advanced Topic/Advance/Linq/BookRepository.cs
+++ 03. C# Advanced Topic/Advance/Linq/BookRepository.cs
@@ -12,7 +12,10 @@ namespace Linq
         {
             return new List<Book>
             {
-                new Book(){Title = "ADO.Net Step by Step", Price = 5},
+                new Book()
+                {
+                    Title = "ADO.Net Step by Step", Price = 5
+                },
                 new Book(){Title = "ASP .NET MVC", Price = 9.99f},
                 new Book(){Title = "ASP .NET Web API", Price = 15},
                 new Book(){Title = "C# Advanced Topics", Price = 5},
