9c24f3c8dd363cb177f83da25fcf40e773450294
Md. Emran Hossen <emranhossen3075@gmail.com>
2025-01-07 16:09:50 +0600

    LINQ

---
+++ 03. C# Advanced Topic/Advance/Linq/BookRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    public class BookRepository
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book(){Title = "ADO.Net Step by Step", Price = 5},
+                new Book(){Title = "ASP .NET MVC", Price = 9.99f},
+                new Book(){Title = "ASP .NET Web API", Price = 15},
+                new Book(){Title = "C# Advanced Topics", Price = 5},
+                new Book(){Title = "C# Advanced Topics", Price = 5}
+
+            };
+        }
+    }
+}
---
+++ 03. C# Advanced Topic/Advance/Linq/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*// LINQ Query Operators
+            var cheaperBooks = 
+                from b in books
+                where b.Price < 10
+                orderby b.Title
+                select b.Title;
+
+            //LINQ Extension Methods [Most Powerfull]
+            var cheapBooks = books
+                                .Where(b => b.Price < 10)
+                                .OrderBy(b => b.Title)
+                                .Select(b =>b.Title);
+           
+
+            foreach(var book in cheapBooks)
+            {
+                Console.WriteLine(book);
+                //Console.WriteLine(book.Title + " " + book.Price);
+            }*/
+
+
+
+
+            var books = new BookRepository().GetBooks();
+            // var book =  books.SingleOrDefault(b => b.Title == "ASP .NET MVC++");
+            var book = books.First(b => b.Title == "C# Advanced Topics");
+
+            //Console.WriteLine(book == null);
+            //Console.WriteLine(book.Title);
+
+            Console.WriteLine(book.Title + " " + book.Price);
+
+        }
+    }
+}
