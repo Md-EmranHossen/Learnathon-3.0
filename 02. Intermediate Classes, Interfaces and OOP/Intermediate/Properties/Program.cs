93f09f7ea7e5695375370eda9b2e350d308fe8e5
Md. Emran Hossen <emranhossen3075@gmail.com>
2025-02-02 10:31:46 +0600

    Properties

---
+++ 02. Intermediate Classes, Interfaces and OOP/Intermediate/Properties/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Properties
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public DateTime BirthDate {  get;private set; }
+
+        public Person(DateTime birthDate)
+        {
+            BirthDate = birthDate;
+        }
+
+        public int Age   // Calculated Age baished on the another property
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - BirthDate;
+                var years = timeSpan.Days / 365;
+                return years;
+            }
+
+        }
+
+
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1982,1,2));
+            Console.WriteLine(person.Age);
+        }
+    }
+    
+}
